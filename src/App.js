4eeb306ddb57d6774ce073768ff28bfe983d309c
Arthur <arthur.robieux@gmail.com>
2018-12-21 15:48:27 +0100

    Add calendar with API team event

--- src/App.js
+++ src/App.js
@@ -1,28 +1,114 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import events from "./events";
+import BigCalendar from "react-big-calendar";
 import './App.css';
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+moment.locale("en");
+
+const localizer = BigCalendar.momentLocalizer(moment) ;
+
+const myEvents = [
+{
+  allDay: true,
+  endDate: new Date('2018-06-02'),
+  startDate: new Date('2018-06-02'),
+  title: 'Event en juin',
+},
+{
+  allDay: false,
+  startDate: new Date('December 09, 2018 11:30:00'),
+  endDate: new Date('December 09, 2018 14:30:00'),
+  title: 'Match foot',
+},
+];
+
 
 class App extends Component {
-  render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            events: myEvents,
+        }
+    }
+
+    eventStyleGetter(event, start, end, isSelected) {
+        console.log(event);
+        var backgroundColor = event.hexColor;
+        var style = {
+            backgroundColor: backgroundColor,
+            borderRadius: '3px',
+            opacity: 0.8,
+            color: 'white',
+            border: '0px',
+            display: 'block'
+        };
+        return {
+            style: style
+        };
+    }
+
+
+    // Get members data from API and save them in state
+    getApiEventsList(){
+
+        const API_URL = 'http://api.local.sporteasy.net:8000/v2.1/teams/6/events/';
+
+        fetch(API_URL, {
+            method: "GET",
+            credentials: 'include',
+        })
+        .then(response =>
+            response.json()
+        )
+        .then(json_response =>
+            this.createEventsFromJson(json_response)
+        )
+    }
+
+    createEventsFromJson(json_response){
+        console.log(json_response);
+
+        var newEvent;
+        var events = [];
+
+        for(var i=0; i<json_response.results.length; i++){
+            console.log(json_response.results[i]);
+
+            newEvent = {
+              allDay: true,
+              endDate: new Date(json_response.results[i].start_at),
+              startDate: new Date(json_response.results[i].start_at),
+              title: <div>
+                         <div className={"event"}>Event {json_response.results[i].id}</div>
+                         <div className={"info_popup"}>{json_response.results[i].category.localized_name}</div>
+                     </div>,
+              hexColor: 'lightsalmon'
+            };
+            events.push(newEvent);
+        }
+        this.setState({events: events});
+    }
+
+    componentDidMount(){
+        this.getApiEventsList();
+    }
+
+
+    render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div style={{ height: 600, width: 900 }}>
+            <BigCalendar
+                localizer={localizer}
+                events={this.state.events}
+                startAccessor="startDate"
+                endAccessor="endDate"
+                eventPropGetter={(this.eventStyleGetter)}
+            />
+        </div>
     );
-  }
+    }
 }
 
 export default App;
